58e5673f650da19edec226b55f1e9995cb4e0fa9
imkiran22 <kiran22292@gmail.com>
2022-05-19 18:38:08 +0530

    Select all through keyboard

--- src/components/TreeList/RenderField/RenderField.tsx
+++ src/components/TreeList/RenderField/RenderField.tsx
@@ -1,3 +1,4 @@
+import { isEmpty } from "lodash";
 import React from "react";
 import { Checkbox } from "../Checkbox";
 import { ITreeList, TreeField } from "../TreeList.types";
@@ -9,7 +10,7 @@ export const RenderField: React.FC<{ fields: ITreeList }> = ({ fields }) => {
       {fields.map((field: TreeField) => (
         <ul key={field.key} className={`ul section-field ${field.key}`}>
           <Checkbox field={field} key={field.key} />
-          {field.fields?.length && <RenderChildren config={field.fields} />}
+          {isEmpty(field.fields?.length) && <RenderChildren config={field.fields as any} />}
         </ul>
       ))}
     </React.Fragment>
